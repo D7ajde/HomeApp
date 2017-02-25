2fd13723a9ac6c0685d6f7d9c36dd93e30d858b1
ASUS-PC\Ilya <D7jade@gmail.com>
2017-02-25 22:37:03 +0300

    Create APp

---
+++ HomeWebAlbum/HomeWebAlbum/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomeWebAlbum.Global" Language="C#" %>
---
+++ HomeWebAlbum/HomeWebAlbum/app/app.module.ts
@@ -0,0 +1,22 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }   from './app.component';
+import { Dashboard } from './dashboard/dashboard.component'
+import { PersonalCabinet } from './personalCabinet/personalCabinet.component'
+
+import { routing } from './core/app.routing';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        routing
+    ],
+    declarations: [AppComponent,
+        Dashboard,
+        PersonalCabinet],
+    bootstrap: [AppComponent]
+    
+})
+
+export class AppModule {}
